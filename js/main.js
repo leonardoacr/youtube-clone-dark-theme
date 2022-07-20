bd48c05998da57f4918a00cca7ea77c40d3ff7ad
Leonardo Rodrigues <102761127+leonardoacr@users.noreply.github.com>
2022-07-20 18:00:52 -0300

    init commit
    First commit for my youtube clone with dark theme.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Detect Menu Button to active or disable left menu nav bar
+
+function inactive_menu_btn() {
+    let inactive_bar = document.querySelector("#inactive_bar");
+    let active_bar = document.querySelector("#active_bar");
+    let top_nav_bar = document.querySelector("#top_nav_bar");
+    let ytb_body = document.querySelector("#ytb_body");
+
+    inactive_bar.style.visibility = 'hidden';
+    active_bar.style.visibility = 'visible';
+    active_bar.style.width = '240px'; // Transition
+    top_nav_bar.style.opacity = 0.3; // Opacity 
+    ytb_body.style.opacity = 0.3; // Opacity 
+}
+
+function active_menu_btn() {
+    inactive_bar.style.visibility = 'visible';
+    active_bar.style.visibility = 'hidden';
+    active_bar.style.width = '0'; // Transition
+    top_nav_bar.style.opacity = 1; // Opacity 
+    ytb_body.style.opacity = 1; // Opacity 
+}
+
+
+function submit_func() {
+    let input_search = document.getElementById("input_search").value;
+    window.open("https://www.youtube.com/results?search_query="+input_search);
+}
